3d68e09f25e39c6df1a7a80d530dd993785290cc
gdulik <tomas.gdula@gmail.com>
2020-11-27 17:14:47 +0100

    dom events forms

---
+++ 17-dom-events-forms/app.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("#signup-form");
+const creditCardInput = document.querySelector("#cc");
+const termsCheckbox = document.querySelector("#tos");
+const vegetableSelect = document.querySelector("#vegetable");
+
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+//   console.log("cc", creditCardInput.value);
+//   console.log("tos", termsCheckbox.checked);
+//   console.log("vegetable", vegetableSelect.value);
+// });
+
+const formData = {};
+for (let input of [creditCardInput, termsCheckbox, vegetableSelect]) {
+  input.addEventListener("input", (e) => {
+    formData[e.target.name] = e.target.value;
+  });
+}
+
+// creditCardInput.addEventListener("input", function (e) {
+//   formData.cc = e.target.value;
+// });
+// termsCheckbox.addEventListener("input", function (e) {
+//   formData.tos = e.target.checked;
+// });
+// vegetableSelect.addEventListener("input", function (e) {
+//   formData.veggie = e.target.value;
+// });
+
+// {
+//   cc: '123456',
+//   tos: true,
+//   veggie: 'carrot'
+// }
