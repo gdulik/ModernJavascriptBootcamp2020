fe4bcc9c26cf76ea18de47d154d09b6fe199051d
gdulik <tomas.gdula@gmail.com>
2020-11-29 21:22:37 +0100

    fetch

--- 20-fetch/app.js
+++ 20-fetch/app.js
@@ -1,16 +1,30 @@
-fetch("https://swapi.dev/api/planetssadad/")
-  .then((res) => {
-    if (!res.ok) {
-      throw new Error(`Status code error: ${res.status}`);
-    } else {
-      res.json().then((data) => {
-        for (let planet of data.results) {
-          console.log(planet.name);
-        }
-      });
-    }
-  })
-  .catch((err) => {
-    console.log("Error");
-    console.log(err);
-  });
+const checkStatusAndParse = (response) => {
+	if (!response.ok) throw new Error(`Status code error: ${response.status}`);
+	return response.json();
+};
+
+const printPlanets = (data) => {
+	console.log('Loaded 10 more planets');
+	for (let planet of data.results) {
+		console.log(planet.name);
+	}
+	return Promise.resolve(data.next);
+};
+
+const fetchNextPlanets = (url = 'https://swapi.dev/api/planets/') => {
+	return fetch(url);
+};
+
+fetchNextPlanets()
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.then(fetchNextPlanets)
+	.then(checkStatusAndParse)
+	.then(printPlanets)
+	.catch((err) => {
+		console.log('Error');
+		console.log(err);
+	});
