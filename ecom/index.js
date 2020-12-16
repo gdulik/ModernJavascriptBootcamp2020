bff3d52a6f7d0df54dac61cb7c25bfee44799c7d
gdulik <tomas.gdula@gmail.com>
2020-12-16 19:29:13 +0100

    ecom image

--- ecom/index.js
+++ ecom/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
+
 const authRouter = require('./routes/admin/auth');
 const productsRouter = require('./routes/admin/products');
 
@@ -8,7 +9,6 @@ const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.multipart)
 app.use(
 	cookieSession({
 		keys: [ 'lkasld235j' ]
